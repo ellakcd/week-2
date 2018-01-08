05c131244e8dbe5a06ff01ed45277ea61b2b29ec
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 12:12:18 -0800

    create get_word_count function

---
+++ wordcount.py
@@ -0,0 +1,32 @@
+# put your code here.
+"""
+initialize dictionary
+open the file
+split it on any space
+iterate over the list
+use .get to check if word is in the dictionary
+if it is, we'll increment the current value by 1
+
+print dictionary with the format, key " " value
+"""
+
+
+def get_word_count(file_name):
+    """Prints word count of given file"""
+    word_count = {}
+    # document = open(file_name)
+
+    with open(file_name) as document:
+
+        for line in document:
+            line = line.rstrip()
+            words = line.split()
+            for word in words:
+                word_count[word] = word_count.get(word, 0) + 1
+
+    for word, count in word_count.iteritems():
+        print word, count
+
+
+get_word_count("test.txt")
+get_word_count("twain.txt")
